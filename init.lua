50fb4c6d91ab2755f3e7d56716d884d408d06b3d
Jairo Sanchez <jairoscz@gmail.com>
2024-01-06 11:15:14 -0600

    Hard-code win32yank clipboard to speed up startup times

--- init.lua
+++ init.lua
@@ -29,6 +29,18 @@ if vim.fn.has("win32") == 1 then
     set_pwsh_opts()
   end
 
+  vim.g.clipboard = {
+    name = 'win32yank',
+    copy = {
+       ["+"] = 'win32yank.exe -i --crlf',
+       ["*"] = 'win32yank.exe -i --crlf',
+     },
+    paste = {
+       ["+"] = 'win32yank.exe -o --lf',
+       ["*"] = 'win32yank.exe -o --lf',
+    },
+    cache_enabled = 0,
+  }
 end
 
 --[[
