e37cece218a55fdcec5fa5cff45fa86e3dd4a640
Jairo Sanchez <jairo@rugged-robotics.com>
2024-01-09 08:22:57 -0600

    Show signature help in insert mode via <C-h>

--- init.lua
+++ init.lua
@@ -614,6 +614,8 @@ local on_attach = function(_, bufnr)
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
 
+  vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, { buffer = bufnr, desc = "Show signature help" })
+
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
