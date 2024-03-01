5e2203bfa533df439d12455ed26471e01ab9ed58
Jairo Sanchez <jairo@rugged-robotics.com>
2024-03-01 09:01:49 -0600

    Start with flash.nvim disabled

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -2,7 +2,11 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {},
+  config = function()
+    local flash = require("flash");
+    flash.setup({})
+    flash.toggle(false)
+  end,
   -- stylua: ignore
   keys = {
     { "<c-s>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
