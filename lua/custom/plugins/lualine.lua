d776913f2e8aa0ebecd4d714f5b4baab6a9ddcbb
Jairo Sanchez <jairo@rugged-robotics.com>
2024-03-29 05:31:28 -0500

    Merge upstream master

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,17 @@
+return {
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      theme = 'tokyonight',
+      -- section_separators = '',
+    },
+    sections = {
+      lualine_c = {
+        'filename',
+        { 'navic', color_correction = nil, navic_opts = nil },
+      },
+    },
+  },
+}
