82f790c5b0ee5b9754b8156ed8a8196b6c42ee48
Jairo Sanchez <jairoscz@gmail.com>
2024-07-25 06:18:48 -0500

    Move lsp and treesitter to own files. Add lazy events.

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,12 +1,15 @@
 return {
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
+    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = { scope = {
-      show_start = false, -- if true, underlines the first line
-      show_end = false, -- if true, underlines the last line
-    }},
+    opts = {
+      scope = {
+        show_start = false, -- if true, underlines the first line
+        show_end = false, -- if true, underlines the last line
+      },
+    },
   },
 }
