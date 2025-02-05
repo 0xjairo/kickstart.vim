c931c564834f033f835267f873f992155ca08c24
Jairo Sanchez <jairo@rugged-robotics.com>
2025-02-05 05:47:34 -0600

    Disable blink completion on cmdline

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -33,7 +33,7 @@ return {
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
         -- optionally disable cmdline completions
-        -- cmdline = {},
+        cmdline = {},
       },
 
       -- experimental signature help support
