ff4fac1cf25d88c44f863b3fa0ce44fd84595437
Jairo Sanchez <jairo@rugged-robotics.com>
2025-06-16 13:10:33 -0500

    Update colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,15 +1,16 @@
 return {
   {
-    'folke/tokyonight.nvim',
+    'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = {
       transparent = true,
     },
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+
+      -- -- choose style 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      -- -- choose style 'kanagawa-wave' (default), 'kanagawa-dragon' (dark), or 'kanagawa-lotus' (light).
+      vim.cmd.colorscheme 'kanagawa-wave'
 
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
